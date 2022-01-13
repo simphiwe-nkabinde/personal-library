44ce33d22adbc39d0d8cd9f1b4d4dd11a5a8f88b
academy_learner <academy_learner@academy-learners-MacBook-Air.local>
2022-01-13 16:37:37 +0200

    simphiwe - angular routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShelfContainerComponent } from './components/shelf-container/shelf-container.component';
+// import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '/', component: AppComponent},
+  { path: 'myshelf', component: ShelfContainerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
