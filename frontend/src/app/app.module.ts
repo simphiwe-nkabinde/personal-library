595a01a6ff17ff595b4282eca8693e63958b0dee
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-05-24 15:47:35 +0200

    create slide animations module and import. add and import stylesheets

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { SlideAnimationsModule } from './slide-animations/slide-animations.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +31,8 @@ import { BookNotesComponent } from './pages/book-notes/book-notes.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SlideAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SlideAnimationsModule } from 'src/app/slide-animations/slide-animations.module';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(public slideAnimation: SlideAnimationsModule) { }
 
   ngOnInit(): void {
+    this.slideAnimation.includeAll();
   }
 
 }
