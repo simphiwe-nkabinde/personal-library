4536b2e230d0a359e18590a396dc0a225f7aaf23
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-05-26 15:10:03 +0200

    implement Sign in with Google OAuth 2.0 feature on frontend

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SlideAnimationsModule } from 'src/app/slide-animations/slide-animations.module';
+declare let google: any;
 
 @Component({
   selector: 'app-landing',
@@ -12,6 +13,34 @@ export class LandingComponent implements OnInit {
 
   ngOnInit(): void {
     this.slideAnimation.includeAll();
+    this.renderSigninButton()
+  }
+
+  renderSigninButton() {
+    google.accounts.id.initialize({
+      client_id: "179512870057-p4q575l99rbb1q2atg5mm061gm72qvtk.apps.googleusercontent.com",
+      callback: this.handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("buttonDiv"),
+      { theme: "outline", size: "large" }  // customization attributes
+    );
+    google.accounts.id.prompt(); // also display the One Tap dialog
+  }
+
+
+  handleCredentialResponse(response: any) {
+    // decodeJwtResponse() is a custom function defined by you
+    // to decode the credential response.
+    console.log(response);
+    // const responsePayload = decodeJwtResponse(response.credential);
+
+    // console.log("ID: " + responsePayload.sub);
+    // console.log('Full Name: ' + responsePayload.name);
+    // console.log('Given Name: ' + responsePayload.given_name);
+    // console.log('Family Name: ' + responsePayload.family_name);
+    // console.log("Image URL: " + responsePayload.picture);
+    // console.log("Email: " + responsePayload.email);
   }
 
 }
